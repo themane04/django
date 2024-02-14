83b2b6def75962f21717bd3338d2673b173df97a
ByteBlazer <marjan@tie-international.com>
2024-02-14 21:35:52 +0100

    fixed login / design improvements

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import get_user_model
 
 
 def register(request):
@@ -19,8 +22,31 @@ def register(request):
     return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-    return render(request, 'users/login.html')
+def user_login(request):
+    User = get_user_model()
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = None
+        if user is not None:
+            user = authenticate(username=user.username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'users/login.html', {'error': 'Invalid email or password.'})
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalid email or password.'})
+    else:
+        return render(request, 'users/login.html')
+
+
+def custom_logout_view(request):
+    logout(request)
+    return redirect('home')  # Redirect to the homepage after logout
 
 
 def index(request):
--- blogWebsite/urls.py
+++ blogWebsite/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from blog.views import register, index, login
+from blog.views import register, index, user_login, custom_logout_view
 
 urlpatterns = [
     path('', index, name='home'),
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),
+    path('logout/', custom_logout_view, name='logout'),
 ]
